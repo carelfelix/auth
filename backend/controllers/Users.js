8658187c6cb6b6fdad971b31bd76bfdb7746db25
Carel Felix <carelfelix@gmail.com>
2024-08-07 16:36:38 +0700

    Setting controller awal untuk Users.js dan penambahan file test request

--- backend/controllers/Users.js
+++ backend/controllers/Users.js
@@ -1,15 +1,46 @@
 import User from "../models/UserModel.js";
+import argon2 from "argon2";
 
-export const getUsers = (req, res) =>{
-
+export const getUsers = async(req, res) =>{
+    try {
+        const response = await User.findAll();
+        res.status(200).json(response);
+    }
+    catch (error) {
+        res.status(500).json({ message: error.message });
+    }
 }
 
-export const getUserById = (req, res) =>{
-
+export const getUserById = async(req, res) =>{
+    try {
+        const response = await User.findOne({
+            where: {
+                uuid: req.params.id
+            }
+        });
+        res.status(200).json(response);
+    }
+    catch (error) {
+        res.status(500).json({ message: error.message });
+    }
 }
 
-export const createUser = (req, res) =>{
-
+export const createUser = async(req, res) =>{
+    const {name, email, password, confPassword, role} = req.body;
+    if(password !== confPassword) return res.status(400).json({msg: "Password dan Confirm Password tidak Cocok!"});
+    const hashedPassword = await argon2.hash(password);
+    try {
+        await User.create({
+            name: name,
+            email: email,
+            password: hashedPassword,
+            role: role
+        });
+        res.status(201).json({msg: "Register Berhasil"});
+    }
+    catch (error) {
+        res.status(400).json({ message: error.message });
+    }
 }
 
 export const updateUser = (req, res) =>{
